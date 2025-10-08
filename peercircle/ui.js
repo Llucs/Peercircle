3af93e08c87f4e641c8cee14ef517abdc3f6c1fd
Llucs <104036495+Llucs@users.noreply.github.com>
2025-10-08 12:02:55 -0300

    Delete peercircle directory

--- peercircle/ui.js
+++
@@ -1,6 +0,0 @@
-function criarMensagem(usuarioID, texto, usuarioIP) {
-    let msgDiv = document.createElement("div");
-    msgDiv.className = "message";
-    msgDiv.innerHTML = `<b>${usuarioID}:</b> ${texto} <span class="denounce-btn" onclick="denunciar('${usuarioID}','${usuarioIP}')">🚨</span>`;
-    document.getElementById("messages").appendChild(msgDiv);
-}
